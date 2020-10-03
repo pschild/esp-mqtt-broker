a840cf86a4da10108ca4cf75b2e5e269973a8a43
Schild, Philippe <Philippe.Schild@adesso.de>
2020-10-03 14:48:14 +0200

    listen for connections via http and ws in parallel

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,88 @@
-// const mongodb = require('mongodb');
-import { createServer } from 'net';
-import * as aedesFn from 'aedes';
-import { PublishPacket } from 'aedes';
+import * as net from 'net';
+import * as http from 'http';
+
+import * as aedes from 'aedes';
+import * as ws from 'websocket-stream';
 import { format } from 'date-fns';
 
+// const mongodb = require('mongodb');
 // const mongoUri = `mongodb://192.168.178.28:27017/`;
 
-const aedes = aedesFn();
-const port = 1883;
+const instance = aedes();
+const httpServer = net.createServer(instance.handle);
+const wsServer = http.createServer();
+
+const PORT_HTTP = 1883;
+const PORT_WS = 1884;
+
+// start http server
+httpServer.listen(PORT_HTTP, () => {
+  log(`HTTP server listening on port ${PORT_HTTP}`);
+});
+
+// start ws server
+ws.createServer({ server: wsServer }, instance.handle as any);
+wsServer.listen(PORT_WS, () => {
+  log(`WS server listening on port ${PORT_WS}`);
+});
+
+instance.on('subscribe', (subscriptions, client) => {
+  // tslint:disable-next-line:max-line-length
+  log(`MQTT client \x1b[32m${client ? client.id : client}\x1b[0m subscribed to topics: ${subscriptions.map(s => s.topic).join(',')} from broker ${instance.id}`);
+});
+
+instance.on('unsubscribe', (subscriptions, client) => {
+  // tslint:disable-next-line:max-line-length
+  log(`MQTT client \x1b[32m${client ? client.id : client}\x1b[0m unsubscribed to topics: ${subscriptions.join(',')} from broker ${instance.id}`);
+});
+
+// fired when a client connects
+instance.on('client', (client) => {
+  log(`Client Connected: \x1b[33m${client ? client.id : client}\x1b[0m to broker ${instance.id}`);
+});
+
+// fired when a client disconnects
+instance.on('clientDisconnect', (client) => {
+  log(`Client Disconnected: \x1b[31m${client ? client.id : client}\x1b[0m to broker ${instance.id}`);
+});
+
+// fired when a message is published
+instance.on('publish', (packet, client) => {
+  // tslint:disable-next-line:max-line-length
+  log(`Client \x1b[31m${client ? client.id : 'BROKER_' + instance.id}\x1b[0m has published ${packet.payload.toString()} on ${packet.topic} to broker ${instance.id}`);
+
+  if (packet.topic.search(/heartbeat/) < 0) {
+    persist(packet);
+  }
+});
 
 function log(logMessage: string) {
   console.log(`${format(new Date(), 'dd.MM.yyyy HH:mm:ss.SSS')}: ${logMessage}`);
 }
 
-aedes.on('subscribe', (subscriptions, client) => {
-  // tslint:disable-next-line:max-line-length
-  log(`MQTT client \x1b[32m${client ? client.id : client}\x1b[0m subscribed to topics: ${subscriptions.map(s => s.topic).join(',')} from broker ${aedes.id}`);
-});
+function persist(packet) {
+  /*
+  mongodb.MongoClient.connect(mongoUri, (error, database) => {
+    if (error != null) {
+      throw error;
+    }
 
-aedes.on('unsubscribe', (subscriptions, client) => {
-  // tslint:disable-next-line:max-line-length
-  log(`MQTT client \x1b[32m${client ? client.id : client}\x1b[0m unsubscribed to topics: ${subscriptions.join(',')} from broker ${aedes.id}`);
-});
+    const db = database.db('mydb');
 
-// fired when a client connects
-aedes.on('client', (client) => {
-  log(`Client Connected: \x1b[33m${client ? client.id : client}\x1b[0m to broker ${aedes.id}`);
-});
+    const collection = db.collection(`mycoll`);
+    collection.createIndex({ topic: 1 });
 
-// fired when a client disconnects
-aedes.on('clientDisconnect', (client) => {
-  log(`Client Disconnected: \x1b[31m${client ? client.id : client}\x1b[0m to broker ${aedes.id}`);
-});
+    const messageObject = {
+      topic: packet.topic,
+      datetime: new Date(),
+      message: packet.payload.toString()
+    };
 
-// fired when a message is published
-aedes.on('publish', async (packet, client) => {
-  // tslint:disable-next-line:max-line-length
-  log(`Client \x1b[31m${client ? client.id : 'BROKER_' + aedes.id}\x1b[0m has published ${packet.payload.toString()} on ${packet.topic} to broker ${aedes.id}`);
-
-  /*if (packet.topic.search(/heartbeat/) < 0) {
-    mongodb.MongoClient.connect(mongoUri, (error, database) => {
+    collection.insertOne(messageObject, (error, result) => {
       if (error != null) {
-        throw error;
+        console.log('ERROR inserting to mongoDb: ' + error);
       }
-
-      const db = database.db('mydb');
-
-      const collection = db.collection(`mycoll`);
-      collection.createIndex({ topic: 1 });
-
-      const messageObject = {
-        topic: packet.topic,
-        datetime: new Date(),
-        message: packet.payload.toString()
-      };
-
-      collection.insertOne(messageObject, (error, result) => {
-        if (error != null) {
-          console.log('ERROR inserting to mongoDb: ' + error);
-        }
-      });
     });
-  }*/
-});
-
-const server = createServer(aedes.handle);
-server.listen(port, () => {
-  console.log('Aedes listening on port:', port);
-  const packet: PublishPacket = {
-    cmd: `publish`,
-    qos: 2,
-    topic: `aedes/hello`,
-    payload: Buffer.from(`I am broker # ${aedes.id}`),
-    retain: false,
-    dup: true
-  };
-  aedes.publish(packet, (err) => console.log(err));
-});
+  });
+  */
+}
